22237c7096e9ca565a781b8c7f9b10ef0a7b8d9f
Darius Capizzi and Ming Wen <darius@>
2016-04-25 16:18:13 -0700

    Add session storage for tasks

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,12 +1,47 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppTest {
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
 
-  @Test
-  public void placeholder(){
-    
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
   }
 
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Task list!");
+  }
+
+  @Test
+    public void taskIsCreatedTest() {
+      goTo("http://localhost:4567/");
+      fill("#description").with("Mow the lawn");
+      submit(".btn");
+      assertThat(pageSource()).contains("Your task has been saved.");
+    }
+
+    @Test
+    public void multipleTasksAreDisplayedTest() {
+      goTo("http://localhost:4567/");
+      fill("#description").with("Mow the lawn");
+      submit(".btn");
+      click("a", withText("Go Back"));
+      fill("#description").with("Buy groceries");
+      submit(".btn");
+      click("a", withText("Go Back"));
+      assertThat(pageSource()).contains("Mow the lawn");
+      assertThat(pageSource()).contains("Buy groceries");
+    }
 }
